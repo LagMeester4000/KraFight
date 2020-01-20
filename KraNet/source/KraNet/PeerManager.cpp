8328851a51dae3ceff01c1aad3582ea11d321ebf
LagMeester4000 <lagmeester4000@gmail.com>
2020-01-20 22:31:02 +0100

    Fix

--- KraNet/source/KraNet/PeerManager.cpp
+++ KraNet/source/KraNet/PeerManager.cpp
@@ -13,3 +13,33 @@ Peer & kra::net::PeerManager::GetLocalPeer()
 	}
 	throw;
 }
+
+kra::net::PeerManager::PeerManager(std::shared_ptr<UDPSocket> Socket, u32 PlayerAmount, bool Spectator)
+{
+}
+
+void kra::net::PeerManager::ProcessPacket(i8 From, const Packet & Pack)
+{
+}
+
+void kra::net::PeerManager::SendToAll(const Packet & Pack)
+{
+}
+
+void kra::net::PeerManager::AddPeer(i8 PlayerNum, const sf::IpAddress & Ip, u16 Port)
+{
+}
+
+void kra::net::PeerManager::AddLocalPeer(u16 Port, i8 PlayerNum)
+{
+}
+
+std::vector<Peer>& kra::net::PeerManager::GetPeers()
+{
+	// TODO: insert return statement here
+}
+
+Peer & kra::net::PeerManager::GetPeer(i8 PlayerNum)
+{
+	// TODO: insert return statement here
+}
